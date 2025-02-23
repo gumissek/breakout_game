eb4e97a25e0a98efc44a3b7c2786b1b393623062
PiotrekB <bilinski.piotr89@gmail.com>
2025-02-23 15:47:17 +0100

    create player

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from turtle import Turtle,Screen
 
 screen = Screen()
+screen.screensize(200,600,bg='black')
 
-tim = Turtle()
+player = Turtle(shape='square')
+player.penup()
+player.color('white')
+player.shapesize(1,3)
+player.setpos(0,-250)
+
+
+
 
 screen.mainloop()
+
+
+# zrobic layout
+# zrobic poruszanie sie gracza
+# zrobic klocki
+# zrobic scoreboarda
+# zrobic odbijanie sie od gracza
+# zrobic niszczenie blokow
+#
